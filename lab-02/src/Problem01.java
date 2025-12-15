58a33556298c6897d08b43a81af7c993d5f696d4
Dilyar444 <gapparov412@gmail.com>
2025-12-15 21:29:28 +0600

    feat:  lab-02 problems ACCEPTED

---
+++ lab-02/src/Problem01.java
@@ -0,0 +1,8 @@
+
+
+public class Problem01 {
+    public static void main(String[] args){
+        String name = "Dilyar";
+        System.out.println(name);
+    }
+}
---
+++ lab-02/src/Problem04.java
@@ -0,0 +1,8 @@
+public class Problem04 {
+    public static void main(String[] args) {
+        String name = "Dilyar";
+        int age = 17;
+        String city = "Bishkek";
+        System.out.println("My name is " + name + ","+ "I am " + age + " years old" + ","+"I live in " + city+'.');
+    }
+}
